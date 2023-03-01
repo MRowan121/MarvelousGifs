c4ba980a2f5808968405ed9abd0f23371bb5b9df
Matt Rowan <matt.rowan121@gmail.com>
2023-03-01 16:24:53 -0500

    Remove unused variables

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
 import './Header.css'
-import headerBackground from '../Images/marvel-header.jpeg'
 
 const Header = () => {
     return (
         <header className="header">
-            {/* <img src={headerBackground} /> */}
+            
         </header>
     )
 }
