77ace351fcb8a5f8c889270066bc96009fa338b0
Matt Rowan <matt.rowan121@gmail.com>
2023-03-01 17:47:03 -0500

    Add api call to get gifs based on search value

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+const getGifs = (searchValue) => {
+    return fetch(`http://api.giphy.com/v1/gifs/search?q=${searchValue}&api_key=QQRengN3S8waij1rwiejGypXXSF6lqN4`)
+    .then(res => {
+            if (!res.ok) {
+                throw new Error('Something went wrong')
+            }
+            return res.json()
+        })
+}
+
+export default { getGifs }
