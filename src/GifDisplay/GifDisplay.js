745b43f5844e3d5d2ce689e1e71ba12cee69ce56
Matt Rowan <matt.rowan121@gmail.com>
2023-03-02 12:36:58 -0500

    Add slider dependency to display gifs

--- src/GifDisplay/GifDisplay.js
+++ src/GifDisplay/GifDisplay.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
-import './GifDisplay.css';
 import { getGifs } from '../apiCalls'
+import { 
+    Swiper, 
+    SwiperSlide
+} from "swiper/react";
+import { 
+    Autoplay, 
+    Grid,
+    Keyboard,
+    Navigation
+} from "swiper";
 
+import './GifDisplay.css';
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 class GifDisplay extends Component {
     constructor() {
@@ -30,22 +44,42 @@ class GifDisplay extends Component {
     render() {
         const gifs = this.state.characterGifs.map((gif, index) => {
             return (
-                <iframe 
-                    key={index} 
-                    src={gif.url} 
-                    width="480" 
-                    height="270" 
-                    frameBorder="0" 
-                    className="giphy-embed" 
-                    style={{'pointerEvents': 'none'}} 
-                    title={gif.title}
-                />
+                <SwiperSlide key={index} className="slide">
+                    <iframe 
+                        title={gif.title}
+                        src={gif.url}
+                        width="100%" 
+                        height="100%" 
+                        frameBorder="0" 
+                        className="giphy-embed" 
+                        style={{'pointerEvents': 'none'}}
+                        allowFullScreen
+                    />
+                </SwiperSlide>
             )
         })
         
         return (
             <section className="gif-display">
-                {gifs}
+                <Swiper
+                    slidesPerView={4}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: true,
+                      }}
+                    grid={{
+                    rows: 2,
+                    }}
+                    keyboard={{
+                        enabled: true,
+                      }}
+                    spaceBetween={30}
+                    navigation={true}
+                    modules={[Autoplay, Grid, Keyboard, Navigation]}
+                    className="mySwiper"
+                >
+                    {gifs}
+                </Swiper>
             </section>
         )
     }
