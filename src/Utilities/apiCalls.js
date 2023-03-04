52ce3ede2074b5fcac780e91f358f785bfff513b
Matt Rowan <matt.rowan121@gmail.com>
2023-03-04 14:22:23 -0500

    Remove commented out header

--- src/Utilities/apiCalls.js
+++ src/Utilities/apiCalls.js
@@ -3,9 +3,7 @@ import characterList from '../Character-Data/characterList';
 const getGifs = (searchValue, limit) => {
     const foundSearch = characterList.find(name => name === searchValue)
     if(foundSearch !== undefined) {
-        return fetch(`http://api.giphy.com/v1/gifs/search?q=${foundSearch}&limit=${limit}&api_key=QQRengN3S8waij1rwiejGypXXSF6lqN4`, {
-            // headers: {"Set-Cookie": "SameSite=Lax"}
-        })
+        return fetch(`http://api.giphy.com/v1/gifs/search?q=${foundSearch}&limit=${limit}&api_key=QQRengN3S8waij1rwiejGypXXSF6lqN4`)
         .then(res => {
             if (!res.ok) {
                 throw new Error('Something went wrong')
