dfea2849b037632e2184eb466daf66657dc85643
Matt Rowan <matt.rowan121@gmail.com>
2023-03-05 14:16:04 -0500

    Comment out myKey variable

--- src/Utilities/apiCalls.js
+++ src/Utilities/apiCalls.js
@@ -1,11 +1,11 @@
 import characterList from '../Character-Data/characterList';
 
 const getGifs = (searchValue, limit) => {
-    const myKey = process.env.REACT_APP_API_KEY_GIF
+    // const myKey = process.env.REACT_APP_API_KEY_GIF
     const foundSearch = characterList.find(name => name === searchValue)
 
     if(foundSearch !== undefined) {
-        return fetch(`http://api.giphy.com/v1/gifs/search?q=${foundSearch}&limit=${limit}&api_key=${myKey}`)
+        return fetch(`http://api.giphy.com/v1/gifs/search?q=${foundSearch}&limit=${limit}&api_key=QQRengN3S8waij1rwiejGypXXSF6lqN4`)
         .then(res => {
             if (!res.ok) {
                 throw new Error('Something went wrong')
